9cabc07b3598e9e6240bde344a9972f5448e6978
Madhumitha M <117455340+Madhumitha2422@users.noreply.github.com>
2023-10-06 21:09:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*================================= typing animation======================================*/
+var typed= new typed(".typing",{ 
+    strings:["","Full Stack Developer","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
